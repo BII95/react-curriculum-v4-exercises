e495c5bb91441181553e0e5b23e9d27862086bab
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-07-26 17:05:12 -0500

    lesson 3

--- src/exercises/lesson-03/BugMutatedState.jsx
+++ src/exercises/lesson-03/BugMutatedState.jsx
@@ -13,8 +13,7 @@ export default function BugMutatedState() {
   let [count, setCount] = useState(0);
 
   function handleAdd() {
-    count++;
-    setCount(count);
+    setCount((prev) => prev + 1);
   }
 
   return (
