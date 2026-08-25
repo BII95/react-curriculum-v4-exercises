1039c042ca0d25e4f4d1c140d50952ffbbd3965f
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-24 23:01:23 -0500

    ?

--- src/exercises/lesson-06/components/userProfile.jsx
+++
@@ -1,3 +0,0 @@
-export default function UserProfile({ studentName }) {
-  return <h2>Welcome, {studentName}</h2>;
-}
--- src/exercises/lesson-06/utils/filterTasks.js
+++
@@ -1,4 +0,0 @@
-export function filterTasks(tasks, filter) {
-  if (filter === 'all') return tasks;
-  return tasks.filter((task) => task.status === filter);
-}
